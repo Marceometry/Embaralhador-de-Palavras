bd0c591121f91c261a010b487be3fe63dc835548
Marceometry <marceometry@gmail.com>
2021-09-27 10:24:07 -0300

    refactor code adding new verifications

--- index.js
+++ index.js
@@ -13,20 +13,51 @@ function createResultArea() {
   div.classList.add('box')
 }
 
-function scrambleText() {
+function generateScrambledText() {
   const textarea = document.querySelector('textarea')
   if (!textarea.value) return
 
   const arrayText = textarea.value.split(' ')
   const length = arrayText.length
+  const scrambledText = scrambleText(arrayText, length)
 
+  const p = document.querySelector('p')
+  p.innerText = scrambledText.join(' ') + '.'
+}
+
+function scrambleText(array, length) {
   let scrambledText = []
+  let arrayText = array
 
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * length - i)
-    const removedWord = arrayText.splice(randomIndex, 1)
-    scrambledText.push(removedWord)
+    const removedWord = arrayText.splice(randomIndex, 1)[0]
+
+    const word = checkAndChange(i, length, removedWord.split(''), scrambledText)
+    scrambledText.push(word)
   }
 
-  document.querySelector('p').innerText = `${scrambledText.join(' ')}.`
+  return scrambledText
+}
+
+function checkAndChange(i, length, wordInArray, scrambledText) {
+  if (i === 0) {
+    wordInArray[0] = wordInArray[0].toUpperCase()
+    return wordInArray.join('')
+  }
+
+  const previousWord = scrambledText[i - 1].split('')
+  const previousWordLastCharacter = previousWord[previousWord.length - 1]
+  if (previousWordLastCharacter === '.') {
+    wordInArray[0] = wordInArray[0].toUpperCase()
+  }
+
+  const currentWordLastCharacter = wordInArray[wordInArray.length - 1]
+  if (i === length - 1) {
+    if (currentWordLastCharacter === '.' || currentWordLastCharacter === ',') {
+      wordInArray.pop()
+    }
+  }
+
+  return wordInArray.join('')
 }
